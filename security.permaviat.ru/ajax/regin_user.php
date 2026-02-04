2728afd56c8cd1277a6debc1df80fc0007352a0a
student-a502 <student-a502@STUD-RM15-A502>
2026-02-04 11:33:50 +0500

    Шаг 10

--- security.permaviat.ru/ajax/regin_user.php
+++ security.permaviat.ru/ajax/regin_user.php
@@ -10,31 +10,36 @@
 	$id = -1;
 	
 	if($user_read = $query_user->fetch_row()) {
-		echo $id;
+    echo $id;
 	} else {
-		$mysqli->query("INSERT INTO `users`(`login`, `password`, `img`, `roll`) VALUES ('".$login."', '".$password."', '', 0)");
-		
-		$query_user = $mysqli->query("SELECT * FROM `users` WHERE `login`='".$login."' AND `password`= '".$password."';");
+		$mysql->query("INSERT INTO `users`('login', `password`, `img`, `roll`) VALUES ('".$login."', '".$password."', '', a)");
+
+		$query_user = $mysql->query("SELECT * FROM `users` WHERE `login`='".$login."' AND `password`= '".$password."';");
 		$user_new = $query_user->fetch_row();
 		$id = $user_new[0];
-		
+
 		$photo = $_FILES['photo'];
-        
-        // Сохраняем файл как user_{$id} с оригинальным расширением
-        $fileInfo = pathinfo($photo['name']);
-        $extension = isset($fileInfo['extension']) ? "." . $fileInfo['extension'] : "";
-        $filename = "user_{$id}{$extension}";
-        $uploadPath = "../img/{$filename}";
-        
-        // Перемещаем файл в нужную директорию
-        if(!move_uploaded_file($photo['tmp_name'], $uploadPath)) {
-            // Ошибка перемещения файла
-            error_log("Не удалось сохранить файл: " . $photo['name']);
-        }
 
-		$mysqli->query("UPDATE `users` SET `img` = '$filename' WHERE `id` = $id");
+		$fileInfo = pathinfo($photo['name']);
+		$extension = isset($fileInfo['extension']) ? "." . $fileInfo['extension'] : "";
 
-		if($id != -1) $_SESSION['user'] = $id; // запоминаем пользователя
+		$allowedExtensions = ['png', '.jpg', '.jpeg'];
+
+		$allowedMimeTypes = ['image/png', 'image/jpeg', 'image/jpg'];
+		$fileMimeType = mime_content_type($photo['tmp_name']);
+
+		if(in_array($extension, $allowedExtensions) && in_array($fileMimeType, $allowedMimeTypes)) {
+		$filename = "user_{$id}{$extension}";
+		$uploadPath = "../img/{$filename}";
+
+		if(!move_uploaded_file($photo['tmp_name'], $uploadPath)) {
+		error_log("Не удалось сохранить файл: " . $photo['name']);
+		}
+
+		$mysql->query("UPDATE `users` SET `img` = '{$filename}' WHERE `id` = {$id}");
+		}
+
+		if($id != -1) $_SESSION['user'] = $id;
 		echo $id;
 	}
 ?>
